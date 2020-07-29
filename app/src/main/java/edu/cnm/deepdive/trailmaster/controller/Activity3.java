a12835a97113779186f7e099d718251e9c291af3
jdrake16 <stovetops111@gmail.com>
2020-07-29 01:24:37 -0600

    1st 3 UI Activities added.

---
+++ app/src/main/java/edu/cnm/deepdive/trailmaster/controller/Activity3.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.trailmaster.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.trailmaster.R;
+
+public class Activity3 extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_3);
+  }
+}
--- app/src/main/java/edu/cnm/deepdive/trailmaster/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/trailmaster/controller/MainActivity.java
@@ -3,6 +3,9 @@ package edu.cnm.deepdive.trailmaster.controller;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +23,36 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     setupObservers();
+
+    Button butt1 = findViewById(R.id.butt1);
+    Button butt2 = findViewById(R.id.butt2);
+    Button butt3 = findViewById(R.id.butt3);
+
+    butt1.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent int1 = new Intent(MainActivity.this,Activity1.class);
+        startActivity(int1);
+      }
+    });
+
+    butt2.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent int2 = new Intent(MainActivity.this,Activity2.class);
+        startActivity(int2);
+      }
+    });
+
+    butt3.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent int3 = new Intent(MainActivity.this,Activity3.class);
+        startActivity(int3);
+      }
+    });
+
+
   }
 
   @Override
@@ -59,4 +92,6 @@ public class MainActivity extends AppCompatActivity {
     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
     startActivity(intent);
   }
+
+
 }
