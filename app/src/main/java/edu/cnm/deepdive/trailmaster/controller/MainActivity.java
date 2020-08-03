63641aca393cbda6cb0c008ad50c4ce61609c4bf
Austin Preston <austinpreston314@gmail.com>
2020-08-03 13:29:08 -0600

    Implemented the google sign page.

--- app/src/main/java/edu/cnm/deepdive/trailmaster/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/trailmaster/controller/MainActivity.java
@@ -55,43 +55,43 @@ public class MainActivity extends AppCompatActivity {
 
   }
 
-//  @Override
-//  public boolean onCreateOptionsMenu(Menu menu) {
-//    super.onCreateOptionsMenu(menu);
-//    getMenuInflater().inflate(R.menu.options, menu);
-//    return true;
-//  }
-//
-//  @Override
-//  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//    boolean handled = true;
-//    //noinspection SwitchStatementWithTooFewBranches
-//    switch (item.getItemId()) {
-//      case R.id.sign_out:
-//        signInService.signOut().addOnCompleteListener((ignored) -> switchToLogin());
-//        break;
-//      default:
-//        handled = super.onOptionsItemSelected(item);
-//    }
-//    return handled;
-//  }
-//
-//  private void setupObservers() {
-//    MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-//    getLifecycle().addObserver(viewModel);
-//    viewModel.getThrowable().observe(this, (throwable) -> {
-//      if (throwable != null) {
-//        Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_LONG).show();
-//      }
-//    });
-//    signInService = GoogleSignInService.getInstance();
-//  }
-//
-//  private void switchToLogin() {
-//    Intent intent = new Intent(this, LoginActivity.class);
-//    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-//    startActivity(intent);
-//  }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    //noinspection SwitchStatementWithTooFewBranches
+    switch (item.getItemId()) {
+      case R.id.sign_out:
+        signInService.signOut().addOnCompleteListener((ignored) -> switchToLogin());
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
+  }
+
+  private void setupObservers() {
+    MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+    getLifecycle().addObserver(viewModel);
+    viewModel.getThrowable().observe(this, (throwable) -> {
+      if (throwable != null) {
+        Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_LONG).show();
+      }
+    });
+    signInService = GoogleSignInService.getInstance();
+  }
+
+  private void switchToLogin() {
+    Intent intent = new Intent(this, LoginActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+    startActivity(intent);
+  }
 
 
 }
