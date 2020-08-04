429f92c5ea3ee9873fedd27993b6e9b5d788ad8b
Austin Preston <austinpreston314@gmail.com>
2020-08-04 17:04:14 -0600

    Swapped Bulletin Board and Stock Trails

--- app/src/main/java/edu/cnm/deepdive/trailmaster/controller/BulletinBoard.java
+++ app/src/main/java/edu/cnm/deepdive/trailmaster/controller/BulletinBoard.java
@@ -1,14 +1,48 @@
 package edu.cnm.deepdive.trailmaster.controller;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 import edu.cnm.deepdive.trailmaster.R;
+import edu.cnm.deepdive.trailmaster.model.Trail;
+import edu.cnm.deepdive.trailmaster.view.TrailAdapter;
+import edu.cnm.deepdive.trailmaster.view.TrailAdapter.OnClickListener;
+import edu.cnm.deepdive.trailmaster.viewmodel.MainViewModel;
 
-public class BulletinBoard extends AppCompatActivity {
+public class BulletinBoard extends AppCompatActivity implements OnClickListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.bulletin_board);
-    }
+        private MainViewModel viewModel;
+        private RecyclerView trails;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.bulletin_board);
+            trails = findViewById(R.id.trails);
+
+
+//    FloatingActionButton buttTrail = findViewById(R.id.add_trail);
+//
+//    buttTrail.setOnClickListener(v -> {
+//      Intent intent = new Intent(Activity4.this,MapFragment.class);
+//      startActivity(intent);
+//
+//
+//    });
+
+            viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+            viewModel.getTrails().observe(this, (trails) -> {
+//      Log.d(getClass().getName(), trails.toString() );
+                TrailAdapter adapter = new TrailAdapter(this, trails, this);
+                this.trails.setAdapter(adapter);
+            });
+
+        }
+
+        @Override
+        public void onClick(View v, int position, Trail trail) {
+            // This method gets called when a user clicks on a trail in the list
+        }
 }
--- app/src/main/java/edu/cnm/deepdive/trailmaster/controller/StockTrails.java
+++ app/src/main/java/edu/cnm/deepdive/trailmaster/controller/StockTrails.java
@@ -12,38 +12,12 @@ import edu.cnm.deepdive.trailmaster.view.TrailAdapter;
 import edu.cnm.deepdive.trailmaster.view.TrailAdapter.OnClickListener;
 import edu.cnm.deepdive.trailmaster.viewmodel.MainViewModel;
 
-public class StockTrails extends AppCompatActivity implements OnClickListener {
-
-  private MainViewModel viewModel;
-  private RecyclerView trails;
+public class StockTrails extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.stock_trails);
-    trails = findViewById(R.id.trails);
-
-
-//    FloatingActionButton buttTrail = findViewById(R.id.add_trail);
-//
-//    buttTrail.setOnClickListener(v -> {
-//      Intent intent = new Intent(Activity4.this,MapFragment.class);
-//      startActivity(intent);
-//
-//
-//    });
-
-    viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-    viewModel.getTrails().observe(this, (trails) -> {
-//      Log.d(getClass().getName(), trails.toString() );
-      TrailAdapter adapter = new TrailAdapter(this, trails, this);
-      this.trails.setAdapter(adapter);
-    });
-
   }
 
-  @Override
-  public void onClick(View v, int position, Trail trail) {
-    // This method gets called when a user clicks on a trail in the list
-  }
 }
